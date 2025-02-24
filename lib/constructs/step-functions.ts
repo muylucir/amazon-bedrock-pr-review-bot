17ceffa2832ae84602ef4c333a6b67ec226ef756
EC2 Default User <ec2-user@ip-10-0-2-70.ap-northeast-2.compute.internal>
2025-02-24 15:40:39 +0000

    modify step function construct

--- lib/constructs/step-functions.ts
+++ lib/constructs/step-functions.ts
@@ -78,24 +78,7 @@ export class ReviewBotStepFunctions extends Construct {
     // 결과 병합 - 수정된 부분
     const mergeResults = new stepfunctions.Pass(this, 'MergeResults', {
       parameters: {
-        'allResults': {
-          'States.ArrayConcat': [
-            '$.classifiedResults.succeeded', 
-            {
-              'States.JsonMerge': [
-                {
-                  'States.JsonMerge': [
-                    '$.retryResults.Items',
-                    '$.retryResults',
-                    false
-                  ]
-                },
-                '[]',
-                false
-              ]
-            }
-          ]
-        }
+        'allResults.$': "States.ArrayConcat($.classifiedResults.succeeded, States.JsonMerge(States.JsonMerge($.retryResults.Items, $.retryResults, false), [], false))"
       },
       resultPath: '$.mergedResults'
     });
